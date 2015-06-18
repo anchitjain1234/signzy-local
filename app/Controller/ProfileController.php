9302fca9b5453c48a018b6396a21638679d5b559
Anchit Jain <anchitjain1234@gmail.com>
2015-06-18 15:41:15 +0530

    Profile Page working

--- app/Controller/ProfileController.php
+++ app/Controller/ProfileController.php
@@ -3,7 +3,7 @@
 class ProfileController extends AppController
 {
   public $layout = 'insidelayout';
-
+  var $helpers = array('UploadPack.Upload');
 
   public function beforeFilter()
 	{
@@ -14,17 +14,30 @@ class ProfileController extends AppController
 
     $uid = CakeSession::read("Auth.User.id");
     $params = array(
-      'fields' => array('name', 'verified'),
       'conditions' => array('id' => $uid),
     );
     $userdata=$this->User->find('first',$params);
     $this->set('name',$userdata['User']['name']);
+    $this->set('userdata',$userdata);
 	}
 
 
   public function index()
   {
-    
+    $this->loadModel('Document');
+    $params = array(
+      'conditions' => array('ownerid' => CakeSession::read("Auth.User.id")),
+      'limit' => 5,
+      'order' => array('created' => -1)
+    );
+    $this->set('uploads',$this->Document->find('all', $params));
+
+    $params = array(
+      'conditions' => array('ownerid' => CakeSession::read("Auth.User.id"),'status'=>"3"),
+      'limit' => 5,
+      'order' => array('created' => -1)
+    );
+    $this->set('disputeduploads',$this->Document->find('all', $params));
   }
 
 }
