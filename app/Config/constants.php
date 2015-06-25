f6fe994b622e3fbc12e65b88895779579d386698
Anchit Jain <anchitjain1234@gmail.com>
2015-06-25 12:07:40 +0530

    made some changes to reolve merge conflicts with master

---
+++ app/Config/constants.php
@@ -0,0 +1,9 @@
+<?php
+
+$config['doc_pending'] = '0';
+$config['doc_completed'] = '1';
+$config['doc_void'] = '2';
+$config['doc_rejected'] = '3';
+$config['user_verified'] = 1;
+$config['user_not_verified'] = 0;
+?>
--- app/Controller/DashboardController.php
+++ app/Controller/DashboardController.php
@@ -17,81 +17,92 @@ class DashboardController extends AppController {
         $this->set('name', $userdata['User']['name']);
     }
 
-    public function index() {
-        $this->layout = 'insidelayout';
-        $this->loadModel('User');
-        $uid = CakeSession::read('Auth.User.id');
+    public function index()
+  {
+  	$this->layout = 'insidelayout';
+    $this->loadModel('User');
+    $uid = CakeSession::read("Auth.User.id");
 
-        $params = array(
-            'fields' => array('verified'),
-            'conditions' => array('id' => $uid),
-        );
-        $userdata = $this->User->find('first', $params);
+    $params = array(
+      'fields' => array('verified'),
+      'conditions' => array('id' => $uid),
+    );
+    $userdata=$this->User->find('first',$params);
 
-        if ($userdata['User']['verified'] === 0) {
-            //$this->Auth->logout();
-            /*
-              Give link to user here for resending verification email.
-             */
-            $this->Session->setFlash(__('Your email hasn\'t been verified.Please
-									 verify it first to continue.'), 'flash_error');
-            //return $this->redirect(array('controller'=>'users','action' => 'index'));
-        }
-        /*
-          Reduce the SQL queries here by combining them as much as possible
-         */
-        $this->loadModel('Document');
-        $params = array(
-            'conditions' => array('ownerid' => CakeSession::read('Auth.User.id'), 'status' => '0'),
-        );
-        $pendingcount = $this->Document->find('count', $params);
-
-        $params = array(
-            'conditions' => array('ownerid' => CakeSession::read('Auth.User.id'), 'status' => '1'),
-        );
-        $completedcount = $this->Document->find('count', $params);
-
-        $params = array(
-            'conditions' => array('ownerid' => CakeSession::read('Auth.User.id'), 'status' => '2'),
-        );
-        $voidcount = $this->Document->find('count', $params);
-
-        $params = array(
-            'conditions' => array('ownerid' => CakeSession::read('Auth.User.id'), 'status' => '3'),
-        );
-        $disputedcount = $this->Document->find('count', $params);
-
-        $this->loadModel('Col');
-        $params = array(
-            'conditions' => array('uid' => CakeSession::read('Auth.User.id')),
-            'fields' => 'did',
-        );
-        $coldata = $this->Col->find('all', $params);
-
-        if ($coldata) {
-            foreach ($coldata as $col):
-                $params = array(
-                    'conditions' => array('id' => $col['Col']['did']),
-                    'fields' => 'status',
-                );
-                $docstatus = $this->Document->find('first', $params);
-
-                if ($docstatus['Document']['status'] === '0') {
-                    $pendingcount += 1;
-                } elseif ($docstatus['Document']['status'] === '1') {
-                    $completedcount += 1;
-                } elseif ($docstatus['Document']['status'] === '2') {
-                    $voidcount += 1;
-                } elseif ($docstatus['Document']['status'] === '3') {
-                    $disputedcount += 1;
-                }
-            endforeach;
-        }
-        $this->set('pendingcount', $pendingcount);
-        $this->set('completedcount', $completedcount);
-        $this->set('voidcount', $voidcount);
-        $this->set('disputedcount', $disputedcount);
+    if($userdata['User']['verified'] === Configure::read('user_not_verified'))
+    {
+      //$this->Auth->logout();
+      /*
+      Give link to user here for resending verification email.
+      */
+      $this->Session->setFlash(__('Your email hasn\'t been verified.Please
+                                   verify it first to continue.'),'flash_error');
+      //return $this->redirect(array('controller'=>'users','action' => 'index'));
     }
+    /*
+    Reduce the SQL queries here by combining them as much as possible
+    */
+    $this->loadModel('Document');
+    $params = array(
+      'conditions' => array('ownerid' => CakeSession::read("Auth.User.id"),'status' => Configure::read('doc_pending')),
+    );
+    $pendingcount = $this->Document->find('count',$params);
+
+    $params = array(
+      'conditions' => array('ownerid' => CakeSession::read("Auth.User.id"),'status' => Configure::read('doc_completed')),
+    );
+    $completedcount = $this->Document->find('count',$params);
+
+    $params = array(
+      'conditions' => array('ownerid' => CakeSession::read("Auth.User.id"),'status' => Configure::read('doc_void')),
+    );
+    $voidcount = $this->Document->find('count',$params);
+
+    $params = array(
+      'conditions' => array('ownerid' => CakeSession::read("Auth.User.id"),'status' => Configure::read('doc_rejected')),
+    );
+    $disputedcount = $this->Document->find('count',$params);
+
+    $this->loadModel('Col');
+    $params = array(
+      'conditions' => array('uid' => CakeSession::read("Auth.User.id")),
+      'fields' => 'did'
+    );
+    $coldata = $this->Col->find('all',$params);
+
+    if($coldata)
+    {
+      foreach($coldata as $col):
+        $params = array(
+          'conditions' => array('id' => $col['Col']['did']),
+          'fields' => 'status'
+        );
+        $docstatus  = $this->Document->find('first',$params);
+
+        if($docstatus['Document']['status'] === Configure::read('doc_pending'))
+        {
+          $pendingcount += 1;
+        }
+        elseif ($docstatus['Document']['status'] === Configure::read('doc_completed'))
+        {
+          $completedcount += 1;
+        }
+        elseif ($docstatus['Document']['status'] === Configure::read('doc_void'))
+        {
+          $voidcount += 1;
+        }
+        elseif ($docstatus['Document']['status'] === Configure::read('doc_rejected'))
+        {
+          $disputedcount += 1;
+        }
+      endforeach;
+    }
+    $this->set('pendingcount',$pendingcount);
+    $this->set('completedcount',$completedcount);
+    $this->set('voidcount',$voidcount);
+    $this->set('disputedcount',$disputedcount);
+
+  }
 
     /*
       These are dummy functions . Will remove them
