a524a7d48bd177dbb4d7dc54d2184abdf003133c
Anchit Jain <anchitjain1234@gmail.com>
2015-07-06 15:59:24 +0530

    authorize and reject buttons disabled during processing

--- app/webroot/js/authorise_user.js
+++ app/webroot/js/authorise_user.js
@@ -1,8 +1,7 @@
 jQuery(document).ready(function () {
     var checked_boxes;
     var ids_checked;
-    console.log('cid');
-    console.log($('.table-hover').attr('id'));
+
     $('.checkbox').change(function () {
 
         ids_checked = [];
@@ -25,17 +24,23 @@ jQuery(document).ready(function () {
     });
 
     $('#authorize_user_btn').click(function () {
+        $('#authorize_user_btn').html('Authorizing ...');
+        $('#authorize_user_btn').attr('disabled', 'disabled');
+        $('#reject_user_btn').attr('disabled', 'disabled');
         $.ajax({
             url: '../authorize.json',
             method: "POST",
             data: {"ids": JSON.stringify(ids_checked), "cid": $('.table-hover').attr('id')}
         }).success(function (res) {
-            if(res['error'] === '')
+            if (res['error'] === '')
             {
                 $('#alertdiv').append("<div id=\"alert\"></div>");
                 $('#alert').addClass("alert alert-danger");
                 $('#alert').html("<a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a>\n\
                                           <strong>Error!</strong>Unknown error.");
+                $('#authorize_user_btn').html('Authorize');
+                $('#authorize_user_btn').removeAttr('disabled');
+                $('#reject_user_btn').removeAttr('disabled');
             }
             else
             {
@@ -46,21 +51,30 @@ jQuery(document).ready(function () {
             $('#alert').addClass("alert alert-danger");
             $('#alert').html("<a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a>\n\
                                           <strong>Error!</strong>Network error.Please check your intenet connection.");
+            $('#authorize_user_btn').html('Authorize');
+            $('#authorize_user_btn').removeAttr('disabled');
+            $('#reject_user_btn').removeAttr('disabled');
         });
     });
-    
+
     $('#reject_user_btn').click(function () {
+        $('#reject_user_btn').html('Rejecting ...');
+        $('#reject_user_btn').attr('disabled', 'disabled');
+        $('#authorize_user_btn').attr('disabled', 'disabled');
         $.ajax({
             url: '../reject.json',
             method: "POST",
             data: {"ids": JSON.stringify(ids_checked), "cid": $('.table-hover').attr('id')}
         }).success(function (res) {
-            if(res['error'] === '')
+            if (res['error'] === '')
             {
                 $('#alertdiv').append("<div id=\"alert\"></div>");
                 $('#alert').addClass("alert alert-danger");
                 $('#alert').html("<a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a>\n\
                                           <strong>Error!</strong>Unknown error.");
+                $('#reject_user_btn').html('Reject');
+                $('#reject_user_btn').removeAttr('disabled');
+                $('#authorize_user_btn').removeAttr('disabled');
             }
             else
             {
@@ -71,6 +85,9 @@ jQuery(document).ready(function () {
             $('#alert').addClass("alert alert-danger");
             $('#alert').html("<a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a>\n\
                                           <strong>Error!</strong>Network error.Please check your intenet connection.");
+            $('#reject_user_btn').html('Reject');
+            $('#reject_user_btn').removeAttr('disabled');
+            $('#authorize_user_btn').removeAttr('disabled');
         });
     });
 });
